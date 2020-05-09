fcf93d4d763790118bc7b3046d46939b0fe8a60b
Piseth <teavpiseth30@gmail.com>
2020-05-09 11:00:46 +0700

    Piseth edit welcome.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
+                    Piseth
                 </div>
 
                 <div class="links">
